e9c245460dcdd5b9f56312ad4cee6c88bea151ad
Niharika Sable <niharikasable@gmail.com>
2023-12-20 14:02:55 +0530

    intialize commit

--- dbconnection/src/aap.js
+++ dbconnection/src/aap.js
@@ -1,14 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
-
-async function main() {
-    mongoose.connect("mongodb://localhost:27017/NeneDB", {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
+mongoose.connect("mongodb://localhost:27017/NeneDB").then(() => {
+  console.log("Connection successful.....");
+}).catch((error) => {
+  console.error(error);
+});
 
