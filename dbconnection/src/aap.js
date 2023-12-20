fa5ebe5fe4d2a43fbf30ee393ec5e55ba8215496
Niharika Sable <niharikasable@gmail.com>
2023-12-20 15:55:04 +0530

    initialize commit

--- dbconnection/src/aap.js
+++ dbconnection/src/aap.js
@@ -1,8 +1,25 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/NeneDB").then(() => {
+mongoose.connect("mongodb+srv://niharikasable:nene@niharikacluster.owgpv5w.mongodb.net/").then(() => {
   console.log("Connection successful.....");
 }).catch((error) => {
   console.error(error);
 });
 
+const todo_schema = new mongoose.Schema({
+  title:String,
+  completed:Boolean
+});
+
+const Todo = mongoose.model("ToDo",todo_schema);
+
+async function insert_todo(){
+  const todo = new Todo({
+    title: "walking",
+    completed: false
+  })
+  await todo.save()
+  console.log("data added sucessfully")
+};
+
+insert_todo();
